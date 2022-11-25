beab5e656a70cded6c7812384dc3c2a9fa85868c
sumi <sumipavib1302@gmail.com>
2022-11-25 10:53:08 +0530

    TicTocToe game

---
+++ src/com/TicTocToeGame/TicTocToeGame.java
@@ -0,0 +1,26 @@
+package com.TicTocToeGame;
+	public class TicTocToeGame {
+		 public static void Displayboard( ) {
+		        char[] board = new char[10];
+		        System.out.println(" ____________");
+		        for (int i = 1; i <= board.length; i++) {
+		            
+		            System.out.print("|___");
+		          if (i % 3 == 0)
+		            {
+		        	  System.out.println("|");
+		            }
+		               
+		        }
+		        System.out.println("|");
+		 }
+		
+	    public static void main(String[] args) {
+	        System.out.println("Welcome in tic tak toe game ");
+	        TicTocToeGame.Displayboard();
+	    
+	   
+	    
+	
+}
+}
